99e3b2125b1c224f63485aff45d757f9da0f2243
MaxFlower <maxtsvetapple@gmail.com>
2015-03-15 23:26:13 +0300

    feat(directive): widget
    -added template, link and controller for returned object
    -changed interval to 2 sec

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 var app = angular.module('refresh_div',[]);
 //make directive with $interval
-    app.directive('tbWidget', function($interval){
-	return function($scope, element, attrs){
-		$scope.$watch(attrs.tbWidget, function(value){
-			$interval (function(){
-				//get random integer
-				var getRandom = (Math.random()*100).toFixed(0);
-				element.text(getRandom);
-				//reset Class for every box
-				element.removeClass('black').removeClass('green').removeClass('red');
-				//set appropriated Class for Element
-				getRandom <21 ? element.addClass('red'): (getRandom > 79 ? element.addClass('green') : element.addClass('black'));						
-			//set interval
-			}, 5000);			
-		});
-	}	
-});
+        
+    app.directive('widget', function($interval){
+		return {
+				template: '<div class="{{valclass}}">{{random}}</div>',
+				replace: true,
+				scope: {},
+				restrict: 'E',
+
+				link: function (scope, element, attributes, controller) {
+		            scope.random = (Math.random()*100).toFixed(0);
+		            scope.random <21 ? scope.valclass = 'lowValue': (scope.random > 79 ? scope.valclass = 'highValue' : scope.valclass = 'midValue');
+		        },
+
+				controller: function($scope, $interval) {
+					$scope.getnumber = $interval(function(){
+						$scope.random = (Math.random()*100).toFixed(0);
+						$scope.random <21 ? $scope.valclass = 'lowValue': ($scope.random > 79 ? $scope.valclass = 'highValue' : $scope.valclass = 'midValue');
+					}, 2000);
+        		}
+		}
+	});
