08280d72fee161e571b6b7343c3325f742261b16
MaxFlowers <maxtsvetapple@gmail.com>
2015-04-03 16:30:39 +0300

    refactor(directive): simplify isolate scope binding
    Added two services GetLimits and GetCurrentValue:
    - GetLimits service define limit for variables
    - GetLimits service set measure unit and accracy for every types of variables
    
    Refactored app structure.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,42 @@
-var app = angular.module('refresh_div',[]);
+(function(){
+	angular.module('refresh_div',[]);
 
-    //make service    
-    app.service('getRandomService', function(){
-    	this.getRandomValue = function(){
-    		return (Math.random()*100).toFixed(0);	
-    	};
-    	this.getValueType = function(value){
-    		if(value<25) return 'lowValue'
-			else if(value>75) return 'highValue'
-			else return 'midValue';	
-    	};
-    });
+    //GetLimits service
+    function sourceLimits () {
+	  this.getSourceLimits = function (sourceName) {	  	
+	  	if (sourceName ==='cpu') return {min: 0, max: 100, measure: '%', accuracy: 0};
+		if (sourceName ==='memoryUsed') return {min: 0, max: 5, measure: 'Gb', accuracy: 2};
+		if (sourceName ==='memoryAvailable') return {min: 0, max: 3, measure: 'Gb', accuracy: 2};  	
+	  };
+	}
+
+    //GetCurrentValue service
+    function sourceData () {
+	  this.getData = function (a, b, c) {
+	  	return (a + Math.random()*b).toFixed(c);	
+	  };
+	}
 
 
-	//make directive with $interval
-    app.directive('widget', ['getRandomService', function($scope, $interval, getRandomService){
-		return {
-				restrict: 'E',
-				template: '<div class="box" ng-class="valueType">{{randomValue}}</div>',
-				replace: true,
-				scope: {},	
-
-				
-				controller: function($scope, $interval, getRandomService) {
-					//Ctrlfunction
-					function valueCicle(){
-						$scope.randomValue =  getRandomService.getRandomValue();
-						$scope.valueType = getRandomService.getValueType($scope.randomValue);						    																						
-					}
-
-					//init
-					valueCicle();					
-
-					//set life interval
-					$interval(valueCicle, 2000);
-
-        		}
-		}
-}]);
+	//App core
+    angular
+    	.module('refresh_div')
+    	.service('GetLimits', sourceLimits)
+    	.service('GetCurrentValue', sourceData)
+	    .directive('widget', function(){
+			return {
+					restrict: 'E',
+					replace: true,					
+					scope: {
+						widgetSource:'@src'
+					},
+					template: '<div class="box"> {{widgetSource + " :  " + sourceData + " " + sourceDef.measure}}</div>',					
+					controller: function($scope, $interval, GetLimits, GetCurrentValue) {						
+						$interval(function(){				
+							$scope.sourceDef = GetLimits.getSourceLimits($scope.widgetSource);							
+							$scope.sourceData = GetCurrentValue.getData($scope.sourceDef.min, $scope.sourceDef.max, $scope.sourceDef.accuracy);							
+						}, 2000);	
+					}       		      		
+			}
+		});
+})();
